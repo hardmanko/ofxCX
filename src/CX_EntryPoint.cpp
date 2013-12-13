bb561b0fd502251c7eb55749dd7c8db266b6c449
Hardman, Kyle (MU-Student) <kh832@mail.missouri.edu>
2013-12-13 11:40:00 -0600

    Standardize on ofGLRenderer

--- src/CX_EntryPoint.cpp
+++ src/CX_EntryPoint.cpp
@@ -52,51 +52,26 @@ int main (void) {
 
 	
 	//It seems like all of this should happen in CX_Display::setup.
-	cout << "Enter \"p\" for programmable renderer, anything else for standard renderer." << endl;
-	string s;
-	cin >> s;
-	if (s == "p") {
-		ofSetCurrentRenderer( (ofPtr<ofBaseRenderer>)(new ofGLProgrammableRenderer), true );
-		ofSetupOpenGL(ofPtr<ofAppBaseWindow>(new ofAppGLFWWindow), 800, 600, OF_WINDOW);
-	} else {
+	//cout << "Enter \"p\" for programmable renderer, anything else for standard renderer." << endl;
+	//string s;
+	//cin >> s;
+	//if (s == "p") {
+	//	ofSetCurrentRenderer( (ofPtr<ofBaseRenderer>)(new ofGLProgrammableRenderer), true );
+	//	ofSetupOpenGL(ofPtr<ofAppBaseWindow>(new ofAppGLFWWindow), 800, 600, OF_WINDOW);
+	//} else {
+
+	ofPtr<ofAppGLFWWindow> window( new ofAppGLFWWindow );
+	window->setNumSamples(4);
+
 		ofSetCurrentRenderer( (ofPtr<ofBaseRenderer>)(new ofGLRenderer), true );
-		ofSetupOpenGL(ofPtr<ofAppBaseWindow>(new ofAppGLFWWindow), 800, 600, OF_WINDOW);
+		ofSetupOpenGL(ofPtr<ofAppBaseWindow>(window), 800, 600, OF_WINDOW);
 
-		ofGetCurrentRenderer()->update(); //This function should be called "setup"
+		ofGetCurrentRenderer()->update(); //This function should be called "setup". This only needs to be called for
 
-		ofDisableAlphaBlending();
-		ofDisableAntiAliasing();
-
+		//ofDisableAlphaBlending(); //This would be more consistent with ofFbo, which does not do alpha blending properly.
 		//ofDisableArbTex(); //Test to see if this helps FBOs in some cases.
-	}
+	//}
 
-
-	/*
-	bool programmableRendererUsed = true;
-
-	try {
-		ofSetCurrentRenderer( (ofPtr<ofBaseRenderer>)(new ofGLProgrammableRenderer), true );
-		ofSetupOpenGL(ofPtr<ofAppBaseWindow>(new ofAppGLFWWindow), 800, 600, OF_WINDOW);
-		ofLogNotice("CX_EntryPoint") << "Sucessfully got an ofGLProgrammableRenderer";
-	} catch (exception e) {
-		ofLogError("CX_EntryPoint") << "Exception thrown while getting ofGLProgrammableRenderer: " << e.what();
-		programmableRendererUsed = false;
-	}
-
-	if (!programmableRendererUsed) {
-		try {
-			ofSetCurrentRenderer( (ofPtr<ofBaseRenderer>)(new ofGLRenderer), true );
-			ofSetupOpenGL(ofPtr<ofAppBaseWindow>(new ofAppGLFWWindow), 800, 600, OF_WINDOW);
-
-			ofGetCurrentRenderer()->update(); //This function should be called "setup"
-			ofLogNotice("CX_EntryPoint") << "Sucessfully got an ofGLRenderer";
-		} catch (exception e) {
-			ofLogFatalError("CX_EntryPoint") << "Exception thrown while getting ofGLRenderer: " << e.what();
-		}
-	}
-	*/
-
-	ofPtr<ofAppBaseWindow> window( ofGetWindowPtr() );
 	window->initializeWindow();
 
 	CX::Private::glfwContext = glfwGetCurrentContext();
