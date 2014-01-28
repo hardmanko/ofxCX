68fcb078c2347a125da0df61c479538fbf1d3a36
Hardman, Kyle (MU-Student) <kh832@mail.missouri.edu>
2014-01-28 15:22:32 -0600

    gabor updates

--- src/CX_DrawingPrimitives.cpp
+++ src/CX_DrawingPrimitives.cpp
@@ -4,13 +4,13 @@ using namespace CX::Draw;
 
 /*!
 This function draws an approximation of a squircle (http://en.wikipedia.org/wiki/Squircle) using Bezier curves.
+The squircle will be centered on 0,0 in the ofPath.
 \param radius The radius of the largest circle that can be enclosed in the squircle.
-\param rotation Rotation of the squircle around its center.
 \param amount The squicliness of the squircle. The default (0.9) seems like a pretty good amount for a good 
 approximation of a squircle, but different amounts can give different sorts of shapes.
 \return An ofPath containing the squircle.
 */
-ofPath CX::Draw::squircleToPath(double radius, double rotation, double amount) {
+ofPath CX::Draw::squircleToPath(double radius, double amount) {
 	ofPath sq;
 
 	ofPoint start, p1, p2, end;
@@ -29,17 +29,25 @@ ofPath CX::Draw::squircleToPath(double radius, double rotation, double amount) {
 		sq.setFilled(false);
 	}
 
-	sq.rotate(rotation, ofVec3f(0, 0, 1));
-
 	return sq;
 }
 
-void squircle(ofPoint center, double radius, double rotation, double amount) {
-	ofPath sq = CX::Draw::squircleToPath(radius, rotation, amount);
+/*
+void squircle(ofPoint center, double radius, double rotationDeg, double amount) {
+	ofPath sq = CX::Draw::squircleToPath(radius, amount);
+	sq.rotate(-rotationDeg, ofVec3f(0, 0, 1));
 	sq.draw(center.x, center.y);
 }
+*/
 
-//The star will be centered on 0,0 in the ofPath.
+/*!
+This draws an N-pointed star to an ofPath. The star will be centered on 0,0 in the ofPath.
+\param numberOfPoints The number of points in the star.
+\param innerRadius The distance from the center of the star to where the inner points of
+the star hit.
+\param outerRadius The distance from the center of the star to the outer points of the star.
+\return An ofPath containing the star.
+*/
 ofPath CX::Draw::starToPath(int numberOfPoints, double innerRadius, double outerRadius) {
 	ofPath star;
 
@@ -65,15 +73,29 @@ ofPath CX::Draw::starToPath(int numberOfPoints, double innerRadius, double outer
 	return star;
 }
 
+/*!
+This draws an N-pointed star.
+\param center The point at the center of the star.
+\param numberOfPoints The number of points in the star.
+\param innerRadius The distance from the center of the star to where the inner points of
+the star hit.
+\param outerRadius The distance from the center of the star to the outer points of the star.
+\param lineColor The color of the lines going around the edge of the star.
+\param fillColor The color used to fill in the center of the star.
+\param lineWidth The width of the lines.
+\param rotationDeg The number of degrees to rotate the star. 0 degrees has one point of the star pointing up. 
+Positive values rotate the star counter-clockwise.
+\return An ofPath containing the star.
+*/
 void CX::Draw::star(ofPoint center, int numberOfPoints, float innerRadius, float outerRadius,
-	ofColor color, ofColor fillColor, float lineWidth, float rotationRad) 
+					ofColor lineColor, ofColor fillColor, float lineWidth, float rotationDeg)
 {
 	ofPath star = CX::Draw::starToPath(numberOfPoints, innerRadius, outerRadius);
-	star.setColor(color);
+	star.setColor(lineColor);
 	star.setFillColor(fillColor);
 	star.setStrokeWidth(lineWidth);
 	
-	star.rotate(rotationRad, ofVec3f(0, 0, 1));
+	star.rotate(-rotationDeg, ofVec3f(0, 0, 1));
 
 	star.draw(center.x, center.y);
 }
@@ -89,45 +111,82 @@ void CX::Draw::centeredString(ofPoint center, string s, ofTrueTypeFont &font) {
 	Draw::centeredString(center.x, center.y, s, font);
 }
 
-
-ofTexture CX::Draw::gaborToTexture (const CX_GaborProperties_t& properties) {
-	//double theta = -properties.angle; //Degrees
-	//double perp = (PI*(theta + 90) / 180);
-
+ofPixels CX::Draw::gaborToPixels (const CX_GaborProperties_t& properties) {
 	double theta = -properties.angle * PI / 180;
 
-	double r = properties.radius;
-	int diameter = 2 * ceil(r);
+	double radius = properties.width / 2;
 
-	double m = tan(theta);
+	double slope = tan(theta);
 
 	ofPixels pix;
-	pix.allocate(diameter, diameter, ofImageType::OF_IMAGE_COLOR_ALPHA);
+	if (properties.apertureType == CX_GaborProperties_t::AP_CIRCLE) {
+		pix.allocate(properties.width, properties.width, ofImageType::OF_IMAGE_COLOR_ALPHA);
+	} else {
+		pix.allocate(properties.width, properties.height, ofImageType::OF_IMAGE_COLOR_ALPHA);
+	}
 	pix.set(3, 0); //Set the alpha channel to 0 (transparent)
 
 	//i indexes y values, j indexes x values
 	for (int i = 0; i < pix.getHeight(); i++) {
 		for (int j = 0; j < pix.getWidth(); j++) {
 
-			ofPoint p(j - (diameter / 2), i - (diameter / 2)); //Center so that x and y are relative to the origin.
+			ofPoint p(j - (pix.getWidth() / 2), i - (pix.getHeight() / 2)); //Center so that x and y are relative to the origin.
 
-			if (p.distance(ofPoint(0,0)) <= r) {
-				double xa = p.y / m; //It should be (p.y - b) / m, but b is 0 because we go through the origin
+			if (properties.apertureType == CX_GaborProperties_t::AP_CIRCLE) {
+				if (p.distance(ofPoint(0, 0)) >(properties.width / 2)) { //Determine radius from the width
+					continue;
+				}
+			} else if (properties.apertureType == CX_GaborProperties_t::AP_RECTANGLE) {
+				//Do nothing, allow pattern to be drawn in entire texture.
+			}
 
-				double hyp = abs(xa - p.x);
-				double distFromA = hyp * sin(theta);
+			double xa = p.y / slope; //It should be ((p.y - b) / slope), but b is 0 because we go through the origin
 
-				if (m == 0) { //Special case for flat lines.
-					distFromA = p.y;
+			double hyp = abs(xa - p.x);
+			double distFromA = hyp * sin(theta);
+
+			if (slope == 0) { //Special case for flat lines.
+				distFromA = p.y;
+			}
+
+			double intensity = 0;
+
+			switch (properties.maskType) {
+			case CX_GaborProperties_t::COSINE_WAVE:
+				intensity = (1.0 + cos((distFromA / properties.period) * 2 * PI)) / 2.0; //Scale to be between 0 and 1
+				break;
+			case CX_GaborProperties_t::SQUARE_WAVE:
+				if (cos((distFromA / properties.period) * 2 * PI) > 0) {
+					intensity = 1;
+				} else {
+					intensity = 0;
 				}
 
-				double intensity = (1.0 + cos((distFromA / properties.period) * 2 * PI)) / 2.0; //Scale to be between 0 and 1
+				break;
+			case CX_GaborProperties_t::TRIANGLE_WAVE:
+				double modulo = abs(fmod(distFromA, properties.period));
 
-				pix.setColor(j, i, ofColor(properties.color.r, properties.color.g, properties.color.b, ofClamp(255 * intensity, 0, 255)));
+				if (modulo >= properties.period / 2) {
+					intensity = modulo / properties.period;
+				} else {
+					intensity = 1 - modulo / properties.period;
+				}
+
+				break;
 			}
+
+			intensity = ofClamp(intensity, 0, 1);
+
+			pix.setColor(j, i, ofColor(properties.color.r, properties.color.g, properties.color.b, properties.color.a * intensity));
+
 		}
 	}
 
+	return pix;
+}
+
+ofTexture CX::Draw::gaborToTexture (const CX_GaborProperties_t& properties) {
+	ofPixels pix = gaborToPixels(properties);
 	ofTexture tex;
 	tex.allocate(pix);
 	tex.loadData(pix);
--- src/CX_DrawingPrimitives.h
+++ src/CX_DrawingPrimitives.h
@@ -10,12 +10,21 @@ namespace CX {
 
 	namespace Draw {
 
-		ofPath squircleToPath(double radius, double rotation, double amount = 0.9);
-		void squircle(ofPoint center, double radius, double rotation, double amount = 0.9);
+		struct CX_PathParams_t {
+			ofColor lineColor;
+			ofColor fillColor;
+			float lineWidth;
+
+			float rotationAmount;
+			ofVec3f rotationAxes;
+		};
+
+		ofPath squircleToPath(double radius, double amount = 0.9);
+		//void squircle(ofPoint center, double radius, double rotationDeg = 0, double amount = 0.9);
 
 		ofPath starToPath(int numberOfPoints, double innerRadius, double outerRadius);
 		void star(ofPoint center, int numberOfPoints, float innerRadius, float outerRadius,
-			ofColor color, ofColor fillColor, float lineWidth = 1, float rotationRad = 0);
+				  ofColor lineColor, ofColor fillColor, float lineWidth = 1, float rotationDeg = 0);
 
 		void centeredString(int x, int y, std::string s, ofTrueTypeFont &font);
 		void centeredString(ofPoint center, string s, ofTrueTypeFont &font);
@@ -24,16 +33,34 @@ namespace CX {
 			CX_GaborProperties_t(void) :
 				color(ofColor::white),
 				angle(0),
-				radius(50),
-				period(30)
+				width(100),
+				height(100),
+				period(30),
+				maskType(CX_GaborProperties_t::COSINE_WAVE),
+				apertureType(CX_GaborProperties_t::AP_CIRCLE)
 			{}
 
-			ofColor color;
+			ofColor color; //The color to use for drawing the gabor. The alpha channel of this color will be the maximum alpha of the pattern.
 			double angle;
-			double radius;
+
+			double width; //If AP_CIRCLE is used, the diameter of the circle is specified by width
+			double height;
+
 			double period;
+
+			enum {
+				COSINE_WAVE,
+				SQUARE_WAVE,
+				TRIANGLE_WAVE
+			} maskType;
+
+			enum {
+				AP_CIRCLE, 
+				AP_RECTANGLE
+			} apertureType;
 		};
 
+		ofPixels gaborToPixels (const CX_GaborProperties_t& properties);
 		ofTexture gaborToTexture (const CX_GaborProperties_t& properties);
 		void gabor (int x, int y, const CX_GaborProperties_t& properties);
 	}
